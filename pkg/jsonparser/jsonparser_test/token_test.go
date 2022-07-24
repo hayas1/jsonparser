97a5c60fa25101ed34e797a46d92153a2bf0b2e6
hayas1 <h4ystack@gmail.com>
2022-07-24 03:46:51 +0000

    add tokenize test

--- pkg/jsonparser/jsonparser_test/token_test.go
+++ pkg/jsonparser/jsonparser_test/token_test.go
@@ -6,30 +6,38 @@ import (
 	jp "github.com/hayas1/jsonparser/pkg/jsonparser"
 )
 
+func testTokenize(tester *testing.T, tokenStrings []string, expectedTokenTypes []jp.TokenType, errorMsg string) {
+	n := len(tokenStrings)
+	if n != len(expectedTokenTypes) {
+		tester.Error("tokenStrings and expectedTokenTypes should have same length")
+		return
+	}
+	isAllOk := true
+	for i := 0; i < n; i++ {
+		isAllOk = isAllOk && jp.Tokenize(tokenStrings[i]).TokenType == expectedTokenTypes[i]
+	}
+	if !isAllOk {
+		tester.Error(errorMsg)
+	}
+}
+
 func TestTokenizeBrace(tester *testing.T) {
 	leftbrace, rightbrace := "{", "}"
-	tl, tr := jp.Tokenize(leftbrace), jp.Tokenize(rightbrace)
-	if !(tl.TokenType == jp.LEFTBRACE && tr.TokenType == jp.RIGHTBRACE) {
-		tester.Error("\"{\" and \"}\" should be brace token")
-	}
+	brace := []string{leftbrace, rightbrace}
+	expected := []jp.TokenType{jp.LEFTBRACE, jp.RIGHTBRACE}
+	testTokenize(tester, brace, expected, "\"{\" and \"}\" should be brace token")
 }
 
 func TestTokenizeWhitespace(tester *testing.T) {
 	space, linefeed, carriageReturn, tab := " ", "\n", "\r\n", "\t"
-	ts, tl, tc, tt := jp.Tokenize(space), jp.Tokenize(linefeed), jp.Tokenize(carriageReturn), jp.Tokenize(tab)
-	isAllWhiteSpace := true
-	for _, t := range []jp.Token{ts, tl, tc, tt} {
-		isAllWhiteSpace = isAllWhiteSpace && t.TokenType == jp.WHITESPACE
-	}
-	if !isAllWhiteSpace {
-		tester.Error("space, linefeed, carriage return, tab should be white space token")
-	}
+	ws := []string{space, linefeed, carriageReturn, tab}
+	expected := []jp.TokenType{jp.WHITESPACE, jp.WHITESPACE, jp.WHITESPACE, jp.WHITESPACE}
+	testTokenize(tester, ws, expected, "space, linefeed, carriage return, tab should be white space token")
 }
 
 func TestTokenizeColon(tester *testing.T) {
 	colon := ":"
-	tc := jp.Tokenize(colon)
-	if !(tc.TokenType == jp.COLON) {
-		tester.Error("\":\" should be brace token")
-	}
+	colons := []string{colon}
+	expected := []jp.TokenType{jp.COLON}
+	testTokenize(tester, colons, expected, "\":\" should be colon token")
 }
