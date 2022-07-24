3e6ab10308524bbbf9a02961c3514d1436dc13bc
hayas1 <h4ystack@gmail.com>
2022-07-24 03:25:01 +0000

    change token type string

--- pkg/jsonparser/token.go
+++ pkg/jsonparser/token.go
@@ -5,20 +5,34 @@ import "fmt"
 type TokenType string
 
 const (
-	LEFTBRACE  TokenType = "{"
-	RIGHTBRACE TokenType = "}"
-	WHITESPACE TokenType = " "
+	LEFTBRACE  TokenType = "LEFTBRACE({)"
+	RIGHTBRACE TokenType = "RIGHTBRACE(})"
+	WHITESPACE TokenType = "WHITESPACE( )"
+	COLON      TokenType = "COLON(:)"
+
+	UNKNOWN TokenType = "UNKNOWN"
 )
 
 type Token struct {
-	Element    string
-	Token_type TokenType
+	Element   string
+	TokenType TokenType
 }
 
-func (t *Token) String() string {
-	return fmt.Sprintf("[\"%s\": %s]", t.Element, t.Token_type)
+func (t Token) String() string {
+	return fmt.Sprintf("<\"%s\": %s>", t.Element, t.TokenType)
 }
 
 func Tokenize(s string) Token {
-	return Token{s, LEFTBRACE}
+	switch s {
+	case "{":
+		return Token{s, LEFTBRACE}
+	case "}":
+		return Token{s, RIGHTBRACE}
+	case " ", "\n", "\r\n", "\t":
+		return Token{s, WHITESPACE}
+	case ":":
+		return Token{s, COLON}
+	default:
+		return Token{s, UNKNOWN}
+	}
 }
