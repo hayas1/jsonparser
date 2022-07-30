ad1ed4dfc05c7e66ffbc68737a42219e84f7cea2
hayas1 <h4ystack@gmail.com>
2022-07-30 08:54:19 +0000

    modify build error message

--- pkg/jsonparser/error.go
+++ pkg/jsonparser/error.go
@@ -13,12 +13,12 @@ type UnexpectedTokenError struct {
 }
 
 func (e *UnexpectedTokenError) Error() string {
-	expectList := make([]string, 0)
+	expectList := make([]string, len(e.expected))
 	for i := 0; i < len(e.expected); i++ {
 		expectList[i] = string(e.expected[i])
 	}
 	expected := fmt.Sprint("", strings.Join(expectList, " or "))
-	return fmt.Sprintf("line %d(col %d): unexpected token %s, expected %s", e.row, e.col, e.token.Element, expected)
+	return fmt.Sprintf("line %d (col %d): unexpected token \"%s\", but expected %s", e.row, e.col, e.token.Element, expected)
 }
 
 func (e *UnexpectedTokenError) AddExpected(expected ...TokenType) {
