089fa33fde37e6c3d50af8b0b6f37fd489dc7a6c
hayas1 <h4ystack@gmail.com>
2022-07-31 00:07:57 +0000

    wip

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/hayas1/jsonparser/pkg/jsonparser"
+	jp "github.com/hayas1/jsonparser/pkg/jsonparser"
 )
 
 func main() {
-	ch := make(chan jsonparser.Token)
-	go jsonparser.Parse("json string", ch)
-	fmt.Println(<-ch)
+	emptyObject := []string{" \t \r\n[ \n \t \t ] \t\t"}
+	parser := jp.NewParser(emptyObject)
+	objectArray, err := parser.ParseArray()
+	fmt.Println(objectArray, err)
+	_, ok := objectArray.Evaluate().([]interface{})
+	fmt.Println(ok)
 }
