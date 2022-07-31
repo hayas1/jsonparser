9c9273097408a65e37706706ccf3ecd840de8e77
hayas1 <h4ystack@gmail.com>
2022-07-31 13:21:23 +0000

    implement file io interface

--- pkg/presence/deserialize.go
+++ pkg/presence/deserialize.go
@@ -1,6 +1,10 @@
 package presence
 
 import (
+	"bufio"
+	"io"
+	"os"
+
 	jp "github.com/hayas1/jsonparser/pkg/jsonparser"
 	"github.com/hayas1/jsonparser/pkg/jsonparser/ast"
 )
@@ -8,4 +12,24 @@ import (
 func Deserialize(jsonString []string) (ast.ValueNode, error) {
 	parser := jp.NewParser(jsonString)
 	return parser.ParseValue()
+}
+
+func DeserializeRead(reader io.Reader) (ast.ValueNode, error) {
+	jsonString := make([]string, 0)
+	r := bufio.NewScanner(reader)
+	for r.Scan() {
+		jsonString = append(jsonString, r.Text())
+	}
+
+	return Deserialize(jsonString)
+}
+
+func DeserializeFile(jsonPath string) (ast.ValueNode, error) {
+	fp, err := os.Open(jsonPath)
+	if err != nil {
+		return ast.ValueNode{}, err
+	}
+	defer fp.Close()
+
+	return DeserializeRead(fp)
 }
--- pkg/presence/serialize.go
+++ pkg/presence/serialize.go
@@ -1,10 +1,33 @@
 package presence
 
 import (
+	"bufio"
+	"io"
+	"os"
+
 	"github.com/hayas1/jsonparser/pkg/jsonparser/ast"
 )
 
 func Serialize(jsonRoot ast.AstNode) string {
 	string := jsonRoot.Dump(0)
 	return string
+}
+
+func SerializeWrite(writer io.Writer, jsonRoot ast.AstNode) error {
+	w := bufio.NewWriter(writer)
+	if _, err := w.WriteString(Serialize(jsonRoot)); err != nil {
+		return err
+	}
+	w.Flush()
+	return nil
+}
+
+func SerializeFile(jsonPath string, jsonRoot ast.AstNode) error {
+	fp, err := os.Create(jsonPath)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+
+	return SerializeWrite(fp, jsonRoot)
 }
